9b356200573a86d53fa8fe450f7e735dbcc25efd
SohibGalaxy <sohibfarhan12@gmail.com>
2024-12-16 18:57:13 +0700

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+function About(){
+    return(
+        <div>
+            <h1 className="text-2x1 font-bold"> About page</h1>
+            <a href="http://localhost:3000" className="text-blue-500 underlie">HomePage</a>
+
+        </div>
+    )
+}
+
+export default About
---
+++ components/HelloWorld.tsx
@@ -0,0 +1,11 @@
+function HelloWorld() {
+  return (
+    <>
+        <h1>Hello world</h1>
+        <p>Selamat Belajar</p>
+        <button>login</button>
+    </>
+  )
+}
+
+export default HelloWorld
